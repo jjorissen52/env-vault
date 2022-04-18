996e8a96dcbf0a245576c09f02130f212a3c1b69
jjorissen52 <jjorissen52@gmail.com>
2022-04-18 11:57:35 -0500

    chore: fix broken test types

--- src/__tests__/test.test.ts
+++ src/__tests__/test.test.ts
@@ -1,7 +1,13 @@
-test("the data is peanut butter", () => {
+test("we did it", () => {
   expect(1).toBe(1);
 });
 
 test("greeting", () => {
   expect("Hello Foo").toBe("Hello Foo");
+});
+
+describe("This test fails", () => {
+  it("will continue to fail until we update the test suite to have actual tests", () => {
+    expect.assertions(1);
+  });
 });
